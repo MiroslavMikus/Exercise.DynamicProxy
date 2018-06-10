6ba6b73cc3d3f161f842f446b97131b00f8ff43e
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-06-10 21:41:01 +0200

    Add unit test project

---
+++ Exercise.DynamicProxy.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercise.DynamicProxy.Test
+{
+    [TestClass]
+    public class FreezableTest
+    {
+        [TestMethod]
+        public void FreezablePerson_Test()
+        {
+            
+        }
+    }
+}
